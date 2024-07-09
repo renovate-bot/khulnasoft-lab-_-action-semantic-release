0b48eb8e7da9180f0491c5bc72082d85a9568ac0
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-09 20:30:38 +0600

    Delete scripts directory
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- scripts/index.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $GITHUB_EVENT_NAME != "push" && $GITHUB_EVENT_NAME != "pull_request" ]]; then
-  echo "::warning title=action-name::action ran on unsupported event ${GITHUB_EVENT_NAME}"
-  exit 0
-fi
-
-echo "bar=baz" >> "${GITHUB_OUTPUT}"
-
